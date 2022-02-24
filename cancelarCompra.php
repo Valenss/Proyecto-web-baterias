03f80bded19116ae40e3cd06a138192550d7b9dd
Valentin Schiaffino <valentinschiaffino@Valentins-MacBook-Air.local>
2022-02-23 21:47:45 -0300

    cambio de no se que

--- cancelarCompra.php
+++ cancelarCompra.php
@@ -4,7 +4,7 @@ header("location: inicio.php");
 $cont=0;
 unset( $_SESSION['producto']);
 unset($_SESSION['contador']);
-unset($_SESSION['cantidad']);
+unset($_SSION['cantidad']);
 unset($_SESSION['productos'][$cont]);
 unset($_SESSION['cantidad'][$cont]);
 session_unset();
